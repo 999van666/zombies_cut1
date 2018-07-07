0615e48fb4fd5e89093ef4b9c4e9f84a97a5263f
vanfix <vanfix@localhost>
2018-07-07 18:56:18 +0300

    ?????? ?????? desktop java.

--- src/ru/zombies_cut/Main.java
+++ src/ru/zombies_cut/Main.java
@@ -1,10 +1,14 @@
 package ru.zombies_cut;
 
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame f = new JFrame ("Java zombies_cut");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(800, 640);
+        f.setVisible(true);
 	}
 
 }
